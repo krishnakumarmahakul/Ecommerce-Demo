c4d535f60654a16298b9fff7164549091e99129a
krishnakumarmahakul <krishnakumahakul@gmail.com>
2024-09-20 16:51:03 +0530

    adding  a new component middle2

---
+++ src/app/home-page/middle2/middle2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-middle2',
+  standalone: true,
+  imports: [],
+  templateUrl: './middle2.component.html',
+  styleUrl: './middle2.component.css'
+})
+export class Middle2Component {
+
+}
