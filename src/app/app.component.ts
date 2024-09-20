1b316c55d50f357473171bd377e0b27f2b798b0d
krishnakumarmahakul <krishnakumahakul@gmail.com>
2024-09-20 16:48:05 +0530

    adding the new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./home-page/nav-bar/nav-bar.component";
+import { MiddleComponent } from "./home-page/middle/middle.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, MiddleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
